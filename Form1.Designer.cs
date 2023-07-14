d6514620f96fbeb104d87baf570db0cea93a2f94
Wendell Mickael Cardoso <135742936+Oiquerida69@users.noreply.github.com>
2023-07-14 17:20:37 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace MaquinadeCoisinhas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_saldo = new System.Windows.Forms.Label();
+            this.panel_Compras = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Bolinho = new System.Windows.Forms.Button();
+            this.btn_Chocolate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_Salvar = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel_Compras.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bem-vindo a maquina de snack\"s";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel1.Controls.Add(this.btn_Salvar);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(11, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(247, 117);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Digite o Valor inicial :";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.CadetBlue;
+            this.panel2.Controls.Add(this.lbl_saldo);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(12, 51);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 55);
+            this.panel2.TabIndex = 3;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = " Saldo :";
+            // 
+            // lbl_saldo
+            // 
+            this.lbl_saldo.AutoSize = true;
+            this.lbl_saldo.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_saldo.Location = new System.Drawing.Point(100, 13);
+            this.lbl_saldo.Name = "lbl_saldo";
+            this.lbl_saldo.Size = new System.Drawing.Size(20, 23);
+            this.lbl_saldo.TabIndex = 1;
+            this.lbl_saldo.Text = "0";
+            // 
+            // panel_Compras
+            // 
+            this.panel_Compras.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.panel_Compras.Controls.Add(this.pictureBox5);
+            this.panel_Compras.Controls.Add(this.pictureBox4);
+            this.panel_Compras.Controls.Add(this.pictureBox3);
+            this.panel_Compras.Controls.Add(this.label6);
+            this.panel_Compras.Controls.Add(this.label5);
+            this.panel_Compras.Controls.Add(this.label4);
+            this.panel_Compras.Controls.Add(this.btn_Chocolate);
+            this.panel_Compras.Controls.Add(this.btn_Bolinho);
+            this.panel_Compras.Controls.Add(this.button1);
+            this.panel_Compras.Location = new System.Drawing.Point(394, 51);
+            this.panel_Compras.Name = "panel_Compras";
+            this.panel_Compras.Size = new System.Drawing.Size(262, 268);
+            this.panel_Compras.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(111, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Doritos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Bolinho
+            // 
+            this.btn_Bolinho.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Bolinho.Location = new System.Drawing.Point(111, 117);
+            this.btn_Bolinho.Name = "btn_Bolinho";
+            this.btn_Bolinho.Size = new System.Drawing.Size(75, 23);
+            this.btn_Bolinho.TabIndex = 1;
+            this.btn_Bolinho.Text = "Bolinho";
+            this.btn_Bolinho.UseVisualStyleBackColor = true;
+            this.btn_Bolinho.Click += new System.EventHandler(this.btn_Bolinho_Click);
+            // 
+            // btn_Chocolate
+            // 
+            this.btn_Chocolate.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Chocolate.Location = new System.Drawing.Point(111, 201);
+            this.btn_Chocolate.Name = "btn_Chocolate";
+            this.btn_Chocolate.Size = new System.Drawing.Size(75, 23);
+            this.btn_Chocolate.TabIndex = 2;
+            this.btn_Chocolate.Text = "Chocolate";
+            this.btn_Chocolate.UseVisualStyleBackColor = true;
+            this.btn_Chocolate.Click += new System.EventHandler(this.btn_Chocolate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(186, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "R$11";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(192, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "R$7";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(192, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "R$9";
+            // 
+            // btn_Salvar
+            // 
+            this.btn_Salvar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Salvar.Location = new System.Drawing.Point(177, 58);
+            this.btn_Salvar.Name = "btn_Salvar";
+            this.btn_Salvar.Size = new System.Drawing.Size(59, 23);
+            this.btn_Salvar.TabIndex = 3;
+            this.btn_Salvar.Text = "Salvar";
+            this.btn_Salvar.UseVisualStyleBackColor = true;
+            this.btn_Salvar.Click += new System.EventHandler(this.btn_Salvar_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Crimson;
+            this.panel4.Controls.Add(this.pictureBox2);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Location = new System.Drawing.Point(-1, -1);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(670, 46);
+            this.panel4.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Crimson;
+            this.panel3.Location = new System.Drawing.Point(-1, 358);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(657, 44);
+            this.panel3.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MaquinadeCoisinhas.Properties.Resources.cola_snack_soda_vending_vending_machine_icon_1320168665103093695;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 234);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 119);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MaquinadeCoisinhas.Properties.Resources.cola_snack_soda_vending_vending_machine_icon_1320168665103093695;
+            this.pictureBox2.Location = new System.Drawing.Point(285, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 37);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::MaquinadeCoisinhas.Properties.Resources._645265a6df1adfffd0988861;
+            this.pictureBox5.Location = new System.Drawing.Point(17, 183);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(88, 57);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::MaquinadeCoisinhas.Properties.Resources.render_doritos_rainbow_55g_03;
+            this.pictureBox4.Location = new System.Drawing.Point(17, 23);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(88, 57);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::MaquinadeCoisinhas.Properties.Resources._63ac2c2620a0ed320dd3529b;
+            this.pictureBox3.Location = new System.Drawing.Point(17, 97);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(88, 59);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(656, 402);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel_Compras);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Maquina de Snank\'s";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel_Compras.ResumeLayout(false);
+            this.panel_Compras.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbl_saldo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel_Compras;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_Chocolate;
+        private System.Windows.Forms.Button btn_Bolinho;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Salvar;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaquinadeCoisinhas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static double saldo;
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(soma < 11)
+            {
+                MessageBox.Show("Você não possui o Saldo necessario para isso");
+            }
+            if (soma >= 11)
+            {
+                soma = soma - 11;
+                lbl_saldo.Text = $"{soma}";
+                MessageBox.Show("Compra realizada com sucesso");
+            }
+
+        }
+        private static double soma;
+        private void btn_Salvar_Click(object sender, EventArgs e)
+        {
+            saldo = double.Parse(textBox1.Text);
+            //lbl_saldo.Text = $"{saldo}";)
+             double Valor = double.Parse(lbl_saldo.Text);
+            
+            soma = saldo + Valor;
+            lbl_saldo.Text = $"{soma}";
+
+            
+
+        }
+
+        private void btn_Bolinho_Click(object sender, EventArgs e)
+        {
+            if(soma < 7)
+            {
+                MessageBox.Show("Você não possui o Saldo necessario para isso");
+            }
+            if(soma >= 7) 
+            {
+                soma = soma - 7;
+                lbl_saldo.Text = $"{soma}";
+                MessageBox.Show("Compra realizada com sucesso");
+            }
+        }
+
+        private void btn_Chocolate_Click(object sender, EventArgs e)
+        {
+            if(soma < 9) 
+            {
+                MessageBox.Show("Você não possui o Saldo necessario para isso");
+            }
+            if(soma >= 9) 
+            {
+                soma = soma - 9;
+                lbl_saldo.Text = $"{soma}";
+                MessageBox.Show("Compra realizada com sucesso");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
